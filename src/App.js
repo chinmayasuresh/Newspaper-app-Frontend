8fa1831f7481cf21c196bab3d64ad3ded44995a0
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-07-27 23:22:51 +0530

    Navbar,router comitted

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './COMPONENTS/Add';
 import Delete from './COMPONENTS/Delete';
 import View from './COMPONENTS/View';
+import Search from './COMPONENTS/Search';
 
 function App() {
   return (
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<View/>}/>
-    <Route path='/' element={<Delete/>}/>
+    <Route path='/delete' element={<Delete/>}/>
+    <Route path='/search' element={<Search/>}/>
+    <Route path='/add' element={<Add/>}/>
+  
   </Routes>
   
   
--- src/COMPONENTS/Nav.jsx
+++ src/COMPONENTS/Nav.jsx
@@ -7,21 +7,25 @@ const Nav = () => {
             <div>
                 <nav class="navbar navbar-expand-lg  bg-body-tertiary" data-bs-theme="dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="/"><em><b>RECIPE APP</b></em></a>
+                        <a class="navbar-brand" href="/"><em><b>SUBSCRIBER APP</b></em></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/add"><b><em>ADD RECIPE</em></b></a>
+                                    <a class="nav-link active" aria-current="page" href="/add"><b><em>ADD</em></b></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/search"><b><em>SEARCH RECIPE</em></b></a>
+                                    <a class="nav-link active" aria-current="page" href="/search"><b><em>SEARCH</em></b></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/view"><b><em>VIEW RECIPE</em></b></a>
+                                    <a class="nav-link active" aria-current="page" href="/"><b><em>VIEW</em></b></a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/delete"><b><em>DELETE</em></b></a>
+                                </li>
+
 
                                 <li class="nav-item">
                                     <a class="nav-link" href="/search"></a>
