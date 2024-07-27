6d87b2790216cf6fdbad8a4659dcb58565b748bf
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-07-27 23:12:01 +0530

    Delete Sreach View comitted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './COMPONENTS/Add';
+import Delete from './COMPONENTS/Delete';
+import View from './COMPONENTS/View';
 
 function App() {
   return (
   <BrowserRouter>
   <Routes>
-    <Route path='/' element={<Add/>}/>
+    <Route path='/' element={<View/>}/>
+    <Route path='/' element={<Delete/>}/>
   </Routes>
   
   
---
+++ src/COMPONENTS/View.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const View = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">SI NO</th>
+      <th scope="col">NAME</th>
+      <th scope="col">PINCODE</th>
+      <th scope="col">PHONE NUMBER</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>016</td>
+      <td>674723876</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>017</td>
+      <td>98465895</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Harry</td>
+      <td>016</td>
+      <td>6734589q</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Mukesh</td>
+      <td>020</td>
+      <td>677487369 </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>DANIEL</td>
+      <td>027</td>
+      <td>9874607863</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Catherine</td>
+      <td>056</td>
+      <td>7849839</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Maria</td>
+      <td>084</td>
+      <td>984387948</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Billy</td>
+      <td>945</td>
+      <td>768487</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Monica</td>
+      <td>078</td>
+      <td>7590830</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Sandra</td>
+      <td>015</td>
+      <td>8649279</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    )
+}
+
+export default View
