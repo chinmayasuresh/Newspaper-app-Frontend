58a6359298aef76f987e8e5ad72b696adae082fa
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-07-27 22:40:38 +0530

    Add comitted

---
+++ src/COMPONENTS/Add.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+
+<div className="container">
+<div className="row">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div className="row g-3">
+     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>SUBCRIBER NAME</b></label>
+<input type="text" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>PINCODE</b></label>
+<input type="text" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>DISTRICT</b></label>
+<input type="text" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>PHONE NUMBER</b></label>
+<input type="number" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>EMAIL ID</b></label>
+
+<input type="email" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>ADDRESS</b></label>
+<input type="text" className="form-control" />
+</div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label"><b>VILLAGE OFFICE</b></label>
+<input type="text" className="form-control" />
+</div>
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+ <button className="btn btn-success">SUBMIT</button>
+</div>
+</div>
+</div>
+</div>
+
+    </div>
+    
+    </div>
+  )
+}
+
+export default Add
+
+
+
+
+
+
